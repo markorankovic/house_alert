0bbc2509a19bf50af4531459667230267de6a427
Marko Ranković <marko@markorankovic.com>
2022-02-15 01:09:02 +0000

    The old expo project has now been fully replaced with react native

--- HouseAlert_ReactNative/__tests__/__unit-tests__/people-context.test.js
+++
@@ -1,6 +0,0 @@
-import { fetchPeopleAsync } from '../../src/context/people-context'
-
-test('Fetch people', async () => {
-    const people = await fetchPeopleAsync({ protocol: "http", ip: "localhost", port: "3000" })
-    expect(people.length).toBe(5)
-})
