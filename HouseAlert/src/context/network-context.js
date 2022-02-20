abbc3bc784d1dcc5860ed51fb809a663974b9936
Marko Ranković <marko@markorankovic.com>
2022-02-20 23:06:05 +0000

    Host IP address caching works

--- HouseAlert/src/context/network-context.js
+++ HouseAlert/src/context/network-context.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const NetworkContext = React.createContext()
 
 export const NetworkProvider = ({children}) => {
     const [hostIP, setHostIP] = useState(null)
+
+    useEffect(() => {
+        if (hostIP) {
+            return
+        }
+        AsyncStorage.getItem('hostIP').then((ip) => {
+            setHostIP(ip)
+        })
+    }, [])
+
+    useEffect(() => {
+        AsyncStorage.setItem('hostIP', hostIP).then()
+    }, [hostIP])
 
     return (
         <NetworkContext.Provider 
