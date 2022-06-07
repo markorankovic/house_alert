ad6a205d279be3516bee09d41ff1cf00c4b9b6dd
Marko Ranković <marko@markorankovic.com>
2022-06-07 17:32:30 +0100

    Status replaces the welcome message

--- HouseAlertServerDesktop/renderer.js
+++ HouseAlertServerDesktop/renderer.js
@@ -1,7 +1,9 @@
 document.getElementById("startButton").addEventListener('click', () => {
     window.myAPI.start()
+    document.getElementById("status").textContent = "Online"
 })
 
 document.getElementById("stopButton").addEventListener('click', () => {
     window.myAPI.stop()
+    document.getElementById("status").textContent = "Offline"
 })
